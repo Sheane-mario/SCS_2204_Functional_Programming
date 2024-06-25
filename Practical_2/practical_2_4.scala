6ff521fea322efb39d920664e105e85b73425bf9
sheanemario <sheanemario7@gmail.com>
2024-06-25 10:37:05 +0530

    new

--- Practical_2/practical_2_4.scala
+++ Practical_2/practical_2_4.scala
@@ -1,16 +1,31 @@
-
 object Main {
-  
-  def attendees(tPrice: Double): Double = 120 + (15 - tPrice)/5*20
-  
-  def expense(attendees: Int): Double = 500.0 + 3.0*attendees
 
-  def income(tPrice: Double): Double = tPrice*attendees(tPrice) 
+  def attendees(tPrice: Double): Double = 120 + (15 - tPrice) / 5 * 20
 
-  def profit(income: Double, expense: Double): Double = income - expense 
+  def expense(attendees: Double): Double = 500.0 + 3.0 * attendees
+
+  def income(tPrice: Double): Double = tPrice * attendees(tPrice)
+
+  def profit(income: Double, expense: Double): Double = income - expense
 
   def main(args: Array[String]): Unit = {
-    
-  }
 
+    var bestPrice = 5.0
+    var maxProfit = Double.MinValue
+
+    for (i <- 5.0 to 100.0 by 1.0) {
+      val currAttendees = attendees(i)
+      val currExpense = expense(currAttendees)
+      val currIncome = income(i)
+      val currProfit = profit(currIncome, currExpense)
+
+      if (currProfit > maxProfit) {
+        bestPrice = i
+        maxProfit = currProfit
+      }
+    }
+
+    println(s"Profit: $maxProfit")
+    println(s"Optimal ticket price: $bestPrice")
+  }
 }
